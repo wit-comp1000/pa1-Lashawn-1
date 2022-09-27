0cdc0ca3ca5e5f8ad68df5981164bb8a72745653
Lashawnatwit <113378759+Lashawn-1@users.noreply.github.com>
2022-09-26 20:42:24 -0400

    Update PA1a.java

--- comp1000_pa1/src/comp1000_pa1/PA1a.java
+++ comp1000_pa1/src/comp1000_pa1/PA1a.java
@@ -1,5 +1,18 @@
 package comp1000_pa1;
-
+import java.util.scanner
 public class PA1a {
+    public static viod main(String[] args){
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Enter number of yards: ")
+    int yards = input.nextInt();
+    System.out.println("Enter number of feet: ");
+    int feet = input.nextInt();
+    System.out.println("Enter number of inches: ")
+    int inches = input.nextInt();
+    
+    int totalinches = (yards *36) + (feet + 12) + inches;
+    System.out.println("Total number of inches" + totalinches);
 
 }
+    }

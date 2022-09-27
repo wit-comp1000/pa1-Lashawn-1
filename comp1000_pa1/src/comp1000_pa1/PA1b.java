2e91a334e33b6c151859f410a6732b6ccd47f883
Lashawnatwit <113378759+Lashawn-1@users.noreply.github.com>
2022-09-26 21:54:30 -0400

    Update PA1b.java

--- comp1000_pa1/src/comp1000_pa1/PA1b.java
+++ comp1000_pa1/src/comp1000_pa1/PA1b.java
@@ -2,7 +2,23 @@ package comp1000_pa1;
 
 public class PA1b {
 	public static void main(String[] args) {
-		// your code here
+	 Scanner input = new Scanner(System.in);
+	
+
+		System.out.println("Enter number of inches: ");
+		int inches = input.nextInt();
+		
+		System.out.println("yard:" + yards);
+		
+		int yards = inches / 36;
+		
+		System.out.println("feet:" + feet);
+		int feet = inches / 12;
+		
+		System.out.println("inches:" + inches);
+		
+		int Inches = inches / 2;
+		
 	}
 
 }
